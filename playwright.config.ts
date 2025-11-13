b8314c2c12762b7908804a5ef778a870c7b03544
Malay Solanki <59390136+malay45@users.noreply.github.com>
2025-11-13 12:36:35 +0530

    Disable parallel tests and adjust worker settings
    Changed test execution settings to disable parallel runs and adjusted worker count for CI.

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,15 +13,22 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['line'], ['allure-playwright'], ['json', { outputFile: 'test-results.json' }]],
+  // reporter: [
+  //   ['list'], // Console reporter
+  //   ['html'], // HTML reporter
+  //   ['json', { outputFile: 'test-results.json' }],
+  //   ['junit', { outputFile: 'results.xml' }], // Add JUnit for CI integration
+  //   ['allure-playwright'],
+  // ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -31,47 +38,72 @@ export default defineConfig({
     trace: 'on-first-retry',
     permissions: ['geolocation'],
     geolocation: { latitude: -33.8698439, longitude: 151.2082848 },
-    screenshot: 'only-on-failure'
+    screenshot: 'only-on-failure',
+    headless: false,
+    launchOptions: {
+      slowMo: 500,
+    },
   },
-  timeout: 2 * 60 * 1000,
-  globalTimeout: 25 * 60 * 1000,
-  expect: {timeout: 120000},
+  timeout: 1000 * 60 * 1000,
+  globalTimeout: 1000 * 60 * 1000,
+  expect: { timeout: 1000 * 60 * 1000 },
   /* Configure projects for major browsers */
   projects: [
     {
+      name: 'desktop',
+      testMatch: ['**/tests/web/**/*.spec.ts'],
+      use: {
+        viewport: { width: 1280, height: 800 },
+        browserName: 'chromium',
+      },
+    },
+    {
+      name: 'desktop',
+      testMatch: ['**/tests/api/**/*.spec.ts'],
+      use: {
+        viewport: { width: 1280, height: 800 },
+        browserName: 'chromium',
+      },
+    },
+    {
+      name: 'mobile',
+      testMatch: ['**/tests/mobile/**/*.spec.ts'],
+      use: {
+        ...devices['iPhone 13'],
+        browserName: 'chromium',
+      },
+    },
+    {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'], },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
     },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
 
     /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    {
+      name: 'google-chrome',
+      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    },
+    {
+      name: 'microsoft-edge',
+      use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    },
+    {
+      name: 'mobile-chrome',
+      use: { ...devices['Pixel 5'] },
+    },
+    {
+      name: 'mobile-safari',
+      use: { ...devices['iPhone 12'] },
+    },
   ],
   globalSetup: "./setup",
 
